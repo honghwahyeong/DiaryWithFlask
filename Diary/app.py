9440705c80168e55f85a6cfee1638210f58db9a8
honghwahyeong <ghdghkgud@dgu.ac.kr>
2021-05-30 22:01:36 +0900

    message창 제외 완료

--- Diary/app.py
+++ Diary/app.py
@@ -21,19 +21,31 @@ def index():
 @app.route("/list")
 def post_list():
     post_list = DB.post_list()
+    if "uid" in session:
+        user = session["uid"]
+    else:
+        user = "Login"
     if post_list == None:
         length = 0
         return redirect(url_for("index"))
     else:
         length = len(post_list)
-    return render_template("post_list.html", post_list=post_list.items(), length=length)
+    return render_template(
+        "post_list.html", post_list=post_list.items(), length=length, user=user
+    )
 
 
 @app.route("/post/<string:pid>")
 def post(pid):
+    if "uid" in session:
+        user = session["uid"]
+    else:
+        user = "Login"
     post, post_id = DB.post_detail(pid)
     image = DB.get_image(pid)
-    return render_template("post_detail.html", post=post, image=image, post_id=post_id)
+    return render_template(
+        "post_detail.html", post=post, image=image, post_id=post_id, user=user
+    )
 
 
 @app.route("/delete_post/<string:pid>")
@@ -51,9 +63,9 @@ def delete_post(pid):
 def logout():
     if "uid" in session:
         session.pop("uid")
-        return redirect(url_for("index"))
+        return redirect(url_for("post_list"))
     else:
-        return redirect(url_for("index"))
+        return redirect(url_for("post_list"))
     return redirect("login.html")
 
 
@@ -70,7 +82,7 @@ def login_done():
     pwd = request.args.get("pwd")
     if DB.login(uid, pwd):
         session["uid"] = uid
-        return redirect(url_for("index"))
+        return redirect(url_for("post_list"))
     else:
         flash("아이디가 없거나 비밀번호가 틀립니다.")
         return redirect(url_for("login"))
@@ -78,7 +90,11 @@ def login_done():
 
 @app.route("/signin")
 def signin():
-    return render_template("signin.html")
+    if "uid" in session:
+        user = session["uid"]
+    else:
+        user = "Login"
+    return render_template("signin.html", user=user)
 
 
 @app.route("/signin_done", methods=["get"])
@@ -88,7 +104,7 @@ def signin_done():
     pwd = request.args.get("pwd")
     name = request.args.get("name")
     if DB.signin(uid, pwd, name, email):
-        return redirect(url_for("index"))
+        return redirect(url_for("post_list"))
     else:
         flash("이미 존재하는 아이디 입니다.")
         return redirect(url_for("signin"))
@@ -102,7 +118,8 @@ def user(uid):
 @app.route("/write")
 def write():
     if "uid" in session:
-        return render_template("write_diary.html")
+        user = session["uid"]
+        return render_template("write_diary.html", user=user)
     else:
         return redirect(url_for("login"))
 
@@ -117,15 +134,18 @@ def write_done():
     file.save(temp.name)
     DB.write_post(title, contents, uid, temp.name)
     os.remove(temp.name)
-    return redirect(url_for("index"))
+    return redirect(url_for("post_list"))
 
 
 @app.route("/edit_post/<string:pid>")
 def edit_post(pid):
+    if "uid" in session:
+        user = session["uid"]
+    else:
+        user = "Login"
     post, post_id = DB.post_detail(pid)
-    if session["uid"] == post["uid"]:
-        print(post["title"])
-        return render_template("edit_post.html", post=post, post_id=post_id)
+    if user == post["uid"]:
+        return render_template("edit_post.html", post=post, post_id=post_id, user=user)
     else:
         return redirect(url_for("post", pid=post_id))
 
